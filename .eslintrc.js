68e9ecb02966d64c04b0abfdff181c08d05f95ca
Jared Truscott <Jtruscott78@gmail.com>
2018-05-15 14:07:04 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-undef': 0,
+    'no-console': 0,
+    'no-return-assign': 0,
+    'no-shadow': 0,
+  },
+};
