2df6735b9f437be7cf2b5a3e4582f0ce633194e1
Jared Truscott <Jtruscott78@gmail.com>
2018-05-17 14:23:16 -0400

    removed material ui sass & added starter template for html

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,19 +1,1 @@
-// material ui lite (comment in as needed)
-// require('../sass/src/mdlComponentHandler.js');
-// require('../sass/src/button/button.js');
-// require('../sass/src/checkbox/checkbox.js');
-// require('../sass/src/icon-toggle/icon-toggle.js');
-// require('../sass/src/menu/menu.js');
-// require('../sass/src/progress/progress.js');
-// require('../sass/src/radio/radio.js');
-// require('../sass/src/slider/slider.js');
-// require('../sass/src/spinner/spinner.js');
-// require('../sass/src/switch/switch.js');
-// require('../sass/src/tabs/tabs.js');
-// require('../sass/src/textfield/textfield.js');
-// require('../sass/src/tooltip/tooltip.js');
-// require('../sass/src/layout/layout.js');
-// require('../sass/src/data-table/data-table.js');
-// require('../sass/src/ripple/ripple.js');
-
 const { $, $$ } = require('./modules/bling');
